278b277391a0ae6cc97c3225786116c28ef7c021
beylsp <beylsp@yahoo.com>
2016-12-12 11:24:55 +0000

    Add PdfPipeline

--- scarab/pipelines.py
+++ scarab/pipelines.py
@@ -1,13 +1,51 @@
+import glob
+import os
 import scrapy.pipelines.images as spi
 
+from PIL import Image
 from scarab.utils import url_to_path
 from scrapy.http import Request
+from scrapy.exceptions import DropItem
 
 
 class ImagesPipeline(spi.ImagesPipeline):
     def get_media_requests(self, item, info):
-        self.pubdate = item['pubdate']
         return [Request(x) for x in item.get(self.images_urls_field, [])]
 
     def file_path(self, request, response=None, info=None):
-        return url_to_path(request.url, self.pubdate)
+        try:
+            return url_to_path(request.url)
+        except ValueError:
+            raise DropItem('Stop ImagesPipeline for %s' % request.url)
+
+
+class PdfPipeline(object):
+    def __init__(self, images_store):
+        self.images_store = images_store
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            crawler.settings.get('IMAGES_STORE')
+        )
+
+    def process_item(self, item, spider):
+        def process_pdf(pubdate):
+            if not pubdate:
+                raise ValueError('Invalid pubdate')
+            m, y = pubdate.split('-')
+            for im in glob.glob(os.path.join(self.images_store, y, m, '*.jpg')):
+                spider.logger.info('Process pdf for %s' % im)
+                n, _ = os.path.splitext(im)
+                try:
+                    image = Image.open(im).save('%s.pdf' % n)
+                except Exception, err:
+                    raise ValueError('Error converting %s: %s' % (im, err))
+
+        # at least one image got updated, process all pdf again
+        if item.get('images'):
+            try:
+                process_pdf(item.get('pubdate'))
+            except Exception, err:
+                raise DropItem('Stop PdfPipeline: %s' %  err)
+        return item
--- scarab/utils.py
+++ scarab/utils.py
@@ -2,7 +2,14 @@ import os
 import urlparse
 
 
-def url_to_path(url, pubdate):
-    urlp = urlparse.urlparse(url)
-    m, y = pubdate.split('-')
-    return '%s/%s/%s' % (y, m, os.path.basename(urlp.path))
+def url_to_path(url):
+    # http://media.motorsportmagazine.com/archive/january-1977/full/27.jpg
+    # --> 1977/january/27.jpg
+    try:
+        urlp = urlparse.urlparse(url)
+        dirname, name = os.path.split(urlp.path)
+        pubdate = dirname.split('/')[2]
+        m, y = pubdate.split('-')
+        return '%s/%s/%s' % (y, m, name)
+    except Exception, err:
+        raise ValueError(err)
